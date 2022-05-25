93098320e5927ae0e539d4f811f2a79dede0a5e8
Mehdi Ouassou <95501612+scavzn@users.noreply.github.com>
2022-05-25 18:37:00 +0200

    Update client.c

--- client.c
+++ client.c
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <sys/sem.h>
 #include <signal.h>
+#include <time.h>
 #define KEY_TURN 115
 #define KEY_SEM1 170
 #define KEY_SEM2 171
@@ -25,15 +26,16 @@ int* event;
 
 void menu() {
   system("clear");
-  printf("Benvenuto player 1");
-  printf("\nCosa vorrebbe fare? (1-4)");
+  printf("Welcome");
+  printf("\nChoose 1-4");
   printf("\n\n1) P1 vs CPU");
   printf("\n2) P1 vs P2");
-  printf("\n3) Regole TRIS");
-  printf("\n4) Terminare");
-  printf("\nScelta: ");
+  printf("\n3) Rules");
+  printf("\n4) Exit");
+  printf("\nChoice: ");
   int cho;
   scanf("%d", &cho);
+
   switch(cho) {
     case 1:
       *event = 3;
@@ -43,7 +45,7 @@ void menu() {
       break;
     case 3:
       system("clear");
-      printf("\nregole\n");
+      printf("\n-The game is played on a grid 3x3 grid.\n-You are X, the opponent is O.\n-Players take turns in inserting their respective mark in the empty squares.\n-The first player to get 3 of their marks in a row (vertically, horizontally or diagonally) wins.\n-When all 9 squares are full, the game ends in a tie.\n-Insert your mark by typing a number from 1 to 9.\n\n\nPRESS ENTER TO RETURN TO THE MENU...\n");
       getchar();
       getchar();
       system("clear");
@@ -55,7 +57,7 @@ void menu() {
       break;
     default:
       system("clear");
-      printf("\nNon e' un numero fra 1-4!\n");
+      printf("\nNOT A NUMBER BETWEEN 1-4!\n");
       getchar();
       getchar();
       system("clear");
@@ -64,6 +66,8 @@ void menu() {
 
   }
 }
+
+
 void fun(int sig) {
     if (player == 0) {
       *event = 135;
@@ -82,7 +86,7 @@ int main() {
 
 	shmturn = shmget(KEY_TURN, sizeof(int), 0600);
   if (shmturn == -1) {
-    printf("Eseguire server prima\n");
+    printf("Server needed first\n");
     exit(0);
   }
 	shmtabella = shmget(KEY_ARRAY, sizeof(char) * 9, 0600);
@@ -127,7 +131,68 @@ int main() {
   }
 
   if(*event == 3){//contro cpu
+    printf("\n");
+        while(1) {
+      	    printf("You're are P[%d], P[%d] turn\n", player, *turn);
+            printf("     |     |     \n");
+            printf("  %c  |  %c  |  %c \n", tabella[0], tabella[1], tabella[2]);
 
+            printf("_____|_____|_____\n");
+            printf("     |     |     \n");
+
+            printf("  %c  |  %c  |  %c \n", tabella[3], tabella[4], tabella[5]);
+
+            printf("_____|_____|_____\n");
+            printf("     |     |     \n");
+
+            printf("  %c  |  %c  |  %c \n", tabella[6], tabella[7], tabella[8]);
+
+            printf("     |     |     \n\n");
+
+      	   printf("\n");
+
+           sleep(1);
+
+           if(*ready >= 3){
+             break;
+           }
+      		 switch(*turn) {
+      			 case 0:
+               while(1){
+                 printf("\nInsert position (1-9): ");
+                 scanf("%d", &choice);
+                 if((choice <= 9) && (choice >= 1)){
+                   if((tabella[choice - 1] == 'O') || (tabella[choice - 1] == 'X')){
+                       continue;
+                   }
+                   break;
+                 }
+               }
+      				 tabella[choice - 1] = 'X';
+               wtabella[choice - 1] = magicCube[choice - 1] * 1;
+      				 *turn = *turn + 1;
+      				 break;
+
+      			case 1://turno bot
+              while(1){
+                srand(time(0));
+                choice = (rand() % 9) + 1;
+                if((choice <= 9) && (choice >= 1)){
+                  if((tabella[choice - 1] == 'O') || (tabella[choice - 1] == 'X')){
+                      continue;
+                  }
+                  break;
+                }
+
+              }
+              tabella[choice - 1] = 'O';
+              wtabella[choice - 1] = magicCube[choice - 1] * 2;
+              *turn = *turn - 1;
+              break;
+            }
+            usleep(1000 * 200);
+            system("clear");
+         }
   } else if (*event == 2){
     while (*ready != 2) {
 
@@ -135,26 +200,34 @@ int main() {
     printf("\n");
     while(1) {
 
-  		printf("You are p[%d], turn[%d]", player, *turn);
-  		for(int i = 0; i < 9; i++) {
-  			if (i % 3 == 0) {
-  				for (int j = 0; j < 2; j++) {
-  					printf("\n");
-  				}
-  			}
+  		printf("You're are P[%d], P[%d] turn", player, *turn);
+      printf("You're are P[%d], P[%d] turn\n", player, *turn);
+      printf("     |     |     \n");
+      printf("  %c  |  %c  |  %c \n", tabella[0], tabella[1], tabella[2]);
 
-  			printf("%c  ", tabella[i]);  //[0,1,2,3,5,6,7,8,9]
-  		}
+      printf("_____|_____|_____\n");
+      printf("     |     |     \n");
 
-  		printf("\n");
+      printf("  %c  |  %c  |  %c \n", tabella[3], tabella[4], tabella[5]);
+
+      printf("_____|_____|_____\n");
+      printf("     |     |     \n");
+
+      printf("  %c  |  %c  |  %c \n", tabella[6], tabella[7], tabella[8]);
+
+      printf("     |     |     \n\n");
+
+     printf("\n");
+
+     sleep(1);
 
       if (*event == 135) {
-        printf("Vittoria a tavolino\n");
+        printf("Win by forfeit\n");
         exit(0);
       }
       else if (*event == 136) {
-        printf("Vittoria a tavolino");
-        printf("\nPremi invio per continuare...");
+        printf("Win by forfeit");
+        printf("\nPress ENTER to continue...");
         getchar();
         getchar();
         *event = 41;
@@ -176,7 +249,7 @@ int main() {
   				switch(*turn) {
   					case 0:
               while(1){
-                printf("insersci posizione (da 1 a 9): ");
+                printf("\nInsert position (1-9): ");
                 scanf("%d", &choice);
                 if((choice <= 9) && (choice >= 1)){
                   if((tabella[choice - 1] == 'O') || (tabella[choice - 1] == 'X')){
@@ -192,7 +265,7 @@ int main() {
     					break;
 
   					case 1:
-    					while((*turn == 1) && (*event > 135)) {
+    					while((*turn == 1) && (*event < 135)) {
     					}
     					break;
 
@@ -204,13 +277,13 @@ int main() {
   				switch(*turn) {
 
   					case 0:
-  					while( (*turn == 0) && (*event > 135)) {
+  					while( (*turn == 0) && (*event < 134)) {
   					}
   					break;
 
   					case 1:
               while(1){
-                printf("insersci posizione (da 1 a 9): ");
+                printf("\nInsert position (1-9): ");
                 scanf("%d", &choice);
                 if((choice <= 9) && (choice >= 1)){
                   if((tabella[choice - 1] == 'O') || (tabella[choice - 1] == 'X')){
@@ -238,17 +311,17 @@ int main() {
     }
     switch(*ready) {
       case 3:
-        printf("\nGiocatore [X] vince");
+        printf("\nPlayer [X] wins");
         break;
       case 4:
-       printf("\nGiocatore [O] vince");
+       printf("\nPlayer [O] wins");
         break;
       case 5:
-        printf("\nPareggio");
+        printf("\nDraw");
         break;
     }
 
-    printf("\nPremi invio per continuare...");
+    printf("\nPress ENTER to continue...");
     getchar();
     getchar();
     *event = 30;
